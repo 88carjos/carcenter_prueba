58057b7d3c7161e198a1858db493c50f4c938539
crodr <calba@netgo.net>
2022-08-19 18:44:52 -0400

    se agrega el proyecto

---
+++ src/main/java/car/center_prueba/carcenter_prueba/CarcenterPruebaApplication.java
@@ -0,0 +1,13 @@
+package car.center_prueba.carcenter_prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcenterPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarcenterPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/car/center_prueba/carcenter_prueba/CarcenterPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package car.center_prueba.carcenter_prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcenterPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
